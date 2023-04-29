7fc0a904fee9dd5113b16ae5b22c8a2e6accff7d
Fernando Morais <fernandomoraisdev@gmail.com>
2023-04-29 10:11:02 -0300

    Primeiro hello world com PHP

---
+++ aula001/aula001.php
@@ -0,0 +1,8 @@
+<?php // abertura do código php 
+echo 'Hello '; //Imprimindo a palavra Hello
+echo 'Wrorld';//Lembrando que ao fim da linha de 
+//código devemos usar sempre ponto e vírgula.
+
+//E ao finalizar o código PHP fechamos com o sinal 
+//de (Interrogação e sinal de maior)
+?>
