dfce1b80b2caefc473aabeafa8379dad273bcf12
Fernando <fernandomoraisdev@gmail.com>
2025-05-24 19:16:53 -0300

    arquivo de estudo

---
+++ aula001/aparando_strings.php
@@ -0,0 +1,8 @@
+<?php
+$csv = ",fernando morais, 36.";
+//tira ponto e virgula ou o que cololar no comando
+//tira do inicio ou do final
+echo strings . phpltrim($csv, ",.") . PHP_EOL;
+echo strings . phprtrim($csv, ",.") . PHP_EOL;
+echo strings . phptrim($csv, ",.") . PHP_EOL;
+
