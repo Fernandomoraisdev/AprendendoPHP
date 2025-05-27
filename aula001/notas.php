4d2fe65d7979d05cbc4b5647611a90d772571396
fernandomoraisdev <fernandomoraisdev@gmail.com>
2025-05-26 22:09:50 -0300

    APrendendo uso da funcao rsort()

---
+++ aula001/notas.php
@@ -0,0 +1,6 @@
+<?php
+$notas = [8, 5, 9, 10, 11, 12, 13, 14, 15, 16, 17 ];
+
+rsort($notas); // Ordena de forma decrescente (r de reverso) do maior para o menor
+
+echo "as tres maiores notas sao $notas[0], $notas[1], $notas[2]";
