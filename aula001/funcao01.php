707d9b5661996348870f42976f9d500c4417b830
fernandomoraisdev <fernandomoraisdev@gmail.com>
2025-06-05 17:50:03 -0300

    correcao

--- aula001/funcao01.php
+++ aula001/funcao01.php
@@ -1,17 +1,20 @@
 <?php
 $numero1 = 110;
 $numero2 = 20;
+
 function operacaoMatematica(int $numero1, int $numero2, string $operacao): float
 {
-   return match($operacao) {
+    return match($operacao) {
         'soma' => $numero1 + $numero2,
         'subtracao' => $numero1 - $numero2,
         'divisao' => $numero1 / $numero2,
         'multiplicacao' => $numero1 * $numero2,
-   };
+        default => throw new InvalidArgumentException("Operação inválida: " . $operacao),
+    };
 }
 
-echo funcao01 . phpoperacaoMatematica($numero1, $numero2, "soma") . PHP_EOL;
-echo funcao01 . phpoperacaoMatematica($numero1, $numero2, "subtracao") . PHP_EOL;
-echo funcao01 . phpoperacaoMatematica($numero1, $numero2, "divisao") . PHP_EOL;
-echo funcao01 . phpoperacaoMatematica($numero1, $numero2, "multiplicacao") . PHP_EOL;
+// Chamadas corretas (sem o "php" antes do nome da função)
+echo operacaoMatematica($numero1, $numero2, "soma") . PHP_EOL;
+echo operacaoMatematica($numero1, $numero2, "subtracao") . PHP_EOL;
+echo operacaoMatematica($numero1, $numero2, "divisao") . PHP_EOL;
+echo operacaoMatematica($numero1, $numero2, "multiplicacao") . PHP_EOL;
