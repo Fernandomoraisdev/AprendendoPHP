57e5f07bc82dd3b882fde09385b708ca5b750470
Fernando Morais <71509217+Fernandomoraisdev@users.noreply.github.com>
2025-06-30 22:10:45 -0300

    commit do dia

--- Aulas002/variavel.php
+++ Aulas002/variavel.php
@@ -2,5 +2,6 @@
 $nome = "Fernando";
 $idade = 36;
 $cidadeNatal = "Rio de Janeiro";
+$bairro: "pilar";
 
-echo "Meu nome e $nome, tenho $idade anos e moro no $cidadeNatal";
+echo "Meu nome e $nome, tenho $idade anos e moro no $cidadeNatal, no bairro $bairro";
