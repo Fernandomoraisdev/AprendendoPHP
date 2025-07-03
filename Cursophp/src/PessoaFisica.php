8e1166e694f41d0b76661e76b75d48936b3e7d8d
Fernando Morais <71509217+Fernandomoraisdev@users.noreply.github.com>
2025-07-02 21:08:58 -0300

    Melhora breve

--- Cursophp/src/PessoaFisica.php
+++ Cursophp/src/PessoaFisica.php
@@ -2,4 +2,21 @@
 class PessoaFisica
 {
   //aqui fica as propriedades e os metodos.
+    public $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
 }
